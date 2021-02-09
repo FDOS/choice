644655e1ee89894b4c378b4e8ad32e3027181d7a
Andrew Bird <ajb@spheresystems.co.uk>
2021-02-09 00:02:02 +0000

    Repository: Add gitattributes and renormalize

--- nls/choice.sv
+++ nls/choice.sv
@@ -1,11 +1,11 @@
-# Translation by Martin Str”mberg <ams@ludd.luth.se>
-0.0:V„ntar tills anv„ndaren trycker p† en tangent fr†n en lista av val
+# Translation by Martin StrÃ¶mberg <ams@ludd.luth.se>
+0.0:VÃ¤ntar tills anvÃ¤ndaren trycker pÃ¥ en tangent frÃ¥n en lista av val
 0.1:val
 0.2:text
-0.3:Specificerar vilka tangenter som „r giltiga. Default „r:
+0.3:Specificerar vilka tangenter som Ã¤r giltiga. Default Ã¤r:
 0.4:Visa inte valen efter prompten
-0.5:Versaler skilda fr†n gemener
-0.6:**Ignorerad, f”r kompatibiltet med MS-DOS
+0.5:Versaler skilda frÃ¥n gemener
+0.6:**Ignorerad, fÃ¶r kompatibiltet med MS-DOS
 0.7:Texten som ska visas som prompt
-0.8:Ljud en signal n„r prompten visas
+0.8:Ljud en signal nÃ¤r prompten visas
 3.0:jn
