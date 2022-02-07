66675d98c81c004292624d42668eebefa5880415
Andrew Bird <ajb@spheresystems.co.uk>
2022-02-07 17:20:46 +0000

    Build: Use tnyprntf submodule

--- build.bat
+++ build.bat
@@ -16,7 +16,6 @@ set CC=wcl
 set COMFLAGS=-mt -lt
 set EXEFLAGS=-mc
 set CFLAGS=-oas -bt=DOS -D__MSDOS__ -zp1 -s -0 -wx -we -zq -fm %EXEFLAGS% -fe=
-set TARGET=choice.exe
 goto doit
 
 :tcc
@@ -37,14 +36,28 @@ set EXEFLAGS=-mc -N -Z -O -k-
 set CFLAGS=-w -M -f- -a- -K -ln %EXEFLAGS% -e
 rem tcc looks for includes from the current directory, not the location of the
 rem file that's trying to include them, so add kitten's location
-set CFLAGS=-I../kitten %CFLAGS%
-set TARGET=choice.exe
+set CFLAGS=-I../kitten -I../tnyprntf %CFLAGS%
 goto doit
 
 :doit
-rem # if you want to build without kitten uncomment the following
-rem set CFLAGS=-DNOCATS %CFLAGS%
-rem set TARGET=_%TARGET%
+set EXTRA_OBJS=
 
-rem We use GNU make for all targets
-make -C src %TARGET%
+set EXTRA_OBJS=%EXTRA_OBJS% tnyprntf.obj
+rem # if you want to build without tnyprntf comment the above and uncomment
+rem the following
+rem set CFLAGS=-DNOPRNTF %CFLAGS%
+
+set EXTRA_OBJS=%EXTRA_OBJS% kitten.obj
+rem # if you want to build without kitten comment the above and uncomment
+rem the following
+rem set CFLAGS=-DNOCATS %CFLAGS%
+
+set UPXARGS=upx --8086 --best
+rem if you don't want to use UPX set
+rem     UPXARGS=-rem
+rem if you use UPX: then options are
+rem     --8086 for 8086 compatibility
+rem   or
+rem     --best for smallest
+
+make -C src
